6a72fb708178db929337b32ced03fab9d95dcd92
Giovanna <giovannadsmessias@outlook.com>
2022-12-12 16:05:59 -0300

    Anatomia das Classes - Java

---
+++ Java-basico/src/MyClass.java
@@ -0,0 +1,16 @@
+public class MyClass {
+    public static void main(String[] args) {
+
+        String primeiroNome = "Giovanna ";
+        String segundoNome = "Messias";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+       public static String nomeCompleto (String primeiroNome, String segundoNome){
+            return primeiroNome.concat("").concat(segundoNome);
+        }
+
+    }
