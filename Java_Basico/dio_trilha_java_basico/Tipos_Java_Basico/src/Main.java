730a140b219832a3b68c0ef6a6fec72cab8d674a
Giovanna <giovannadsmessias@outlook.com>
2022-12-21 11:44:15 -0300

    Tipos e Variaveis - Constantes
    Constantes, são valores armazenados em memória que não podem ser modificados depois de declarados. Em Java, esses valores são representados pela palavra reservada final, seguida do tipo.

--- Java_Basico/dio_trilha_java_basico/Tipos_Java_Basico/src/Main.java
+++ Java_Basico/dio_trilha_java_basico/Tipos_Java_Basico/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        int idade = 5;
+
+        idade = 10;
+
+        final double valor_Pi = 3.14; //valor que não pode ser modificado depois de declarado//
+        
     }
-}
+
+    }
