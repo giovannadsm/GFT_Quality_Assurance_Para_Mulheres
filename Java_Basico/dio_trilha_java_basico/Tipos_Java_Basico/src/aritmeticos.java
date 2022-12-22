9c507fc6cc6c54a66bec03a9e073bc3c7af4c2c3
Giovanna <giovannadsmessias@outlook.com>
2022-12-22 11:19:55 -0300

    Operadores - Aritméticos
    O operador aritmético, é utilizado para realizar operações matemáticas entre valores numéricos, podendo se tornar ou não uma expressão mais complexa.
    Os operadores aritméticos são: + (adição), - (subtração), * (multiplicação) e / (divisão).

---
+++ Java_Basico/dio_trilha_java_basico/Tipos_Java_Basico/src/aritmeticos.java
@@ -0,0 +1,15 @@
+public class aritmeticos {
+    public static void main(String[] args) {
+
+        //classe Operadores.java
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        System.out.println(soma + subtracao + multiplicacao + divisao + modulo + resultado);
+
+    }
+}
