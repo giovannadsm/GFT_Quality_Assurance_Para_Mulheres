1c97dbda041749b173bb270d297ebd852984d146
Giovanna <giovannadsmessias@outlook.com>
2023-01-02 22:55:22 -0300

    Metodos - Exemplos sugeridos para nomenclatura de métodos
    Critério de nomeação de Métodos
    Esses critérios não são obrigatórios, mas é recomendável que sejam seguidos, pois essas convenções facilitam a vida dos programadores ao trabalharem em códigos de forma colaborativa. Ao seguir estas convenções, tornamos o código mais legível para nós e também para outras pessoas. Para métodos, os critérios são:
    Deve ser nomeado como verbo;
    Seguir o padrão camelCase (Todas as letras minúsculas com a exceção da primeira letra da segunda palavra).

---
+++ Java_Basico/dio_trilha_java_basico/Tipos_Java_Basico/src/metodos/nomeacao_de_metodos.java
@@ -0,0 +1,22 @@
+package metodos;
+
+public class nomeacao_de_metodos {
+
+    public static void main(String[] args) {
+
+        somar( int n1, int n2){
+        }
+
+        abrirConexao() {
+        }
+
+        concluirProcessamento() {
+        }
+
+        findById( int id){
+        } // não se assuste, você verá muito método em inglês em sua jornada
+
+        calcularImprimir() {
+        } // há algo de errado neste método, ele deveria ter uma única finalidade
+    }
+}
